0e2dcf4edcde3ecc311ac4ec53c62da220876803
h22j2in <gmlwls0204@naver.com>
2018-10-29 17:42:26 +0900

    model2

---
+++ day46/myProject/src/test/java/kr/or/kosta/myProject/AppTest.java
@@ -0,0 +1,23 @@
+package kr.or.kosta.myProject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest{
+	
+	@Before
+	public void init() {
+	}
+	
+	@After
+	public void finish() {
+	}
+	
+	@Test
+	public void testApp() {
+		System.out.println("이것이 maven 입니다..");
+	}
+	
+   
+}
